5e1da9d1e397352ac4699ead6f5684e688865739
Andrew D. Yates <andrewyates.name@gmail.com>
2012-07-09 04:17:54 -0400

    working histogram generation

--- enriched.py
+++ enriched.py
@@ -1,5 +1,7 @@
 from util import *
 from py_symmetric_matrix import *
+import random
+
 
 class Enriched(object):
 
@@ -13,16 +15,17 @@ class Enriched(object):
     self.pairs = []
     self.pairs_hash = set()
     
-    for pair in pairs:
-      x, y = pair
+    for x, y in pairs:
       x, y = clean(x), clean(y)
       x, y = sorted((x,y))
       if not (x and y) or x == y:
         continue
-      self.genes.add(x)
-      self.genes.add(y)
-      self.pairs.append((x,y))
-      self.pairs_hash.add(self._hash(x,y))
+      q = self._hash(x,y)
+      if not q in self.pairs_hash:
+        self.genes.add(x)
+        self.genes.add(y)
+        self.pairs.append((x,y))
+        self.pairs_hash.add(q)
 
   def _hash(self, x,y):
     return "%s,%s" % (x,y)
@@ -36,7 +39,7 @@ class Enriched(object):
     return self._hash(x,y) in self.pairs_hash
 
   def indices(self, varlist):
-    """Return a list of indices for sym dependency matrix using varlist.
+    """Return indices of self.pairs for sym dependency matrix indexed by varlist.
     """
     n = len(varlist)
     a = []
@@ -50,6 +53,7 @@ class Enriched(object):
       idx = sym_idx(i,j,n)
       a.append(idx)
     return a
+    
 
 
 class PINAEnriched(Enriched):
@@ -70,3 +74,26 @@ class PINAEnriched(Enriched):
   def __init__(self, fp):
     """Initialize from new open fp to PINA formatted file."""
     super(PINAEnriched, self).__init__(pairs=self._pair_gen(fp))
+
+
+    
+class RandomPINAEnriched(PINAEnriched):
+  """Generate randomized enrichment set from a PINA set.
+
+  Rather hacky...
+  """
+  def __init__(self, fp):
+    super(RandomPINAEnriched, self).__init__(fp)
+    # replace self.pairs and self.pairs_hash with randomly generated sets of equal length
+    n_pairs = len(self.pairs_hash)
+    n = len(self.genes)
+    gene_list = list(self.genes)
+    self.pairs = []
+    self.pairs_hash = set()
+    
+    idxs = random.sample(xrange(int(n*(n-1)/2)), n_pairs)
+    for i in idxs:
+      x, y = inv_sym_idx(i, n)
+      gene_x, gene_y = sorted((gene_list[x], gene_list[y]))
+      self.pairs.append((gene_x, gene_y))
+      self.pairs_hash.add("%s,%s"%(gene_x, gene_y))
